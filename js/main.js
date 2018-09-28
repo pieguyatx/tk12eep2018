4ab696e8896a048c60ec4d9c171ef9bc15f1c4ea
Pius Wong <pius@pioslabs.com>
2018-09-28 15:02:24 -0500

    More interactive nav w/ JS classes

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+  init_navbar(); // initialize navbar hover effects
+};
+
+function init_navbar(){
+  document.getElementById("nav-listen").addEventListener("mouseenter", navListenActive);
+  document.getElementById("nav-listen").addEventListener("mouseleave", navListenInactive);
+  document.getElementById("nav-about").addEventListener("mouseenter", navAboutActive);
+  document.getElementById("nav-about").addEventListener("mouseleave", navAboutInactive);
+
+  function navListenActive(){
+    document.getElementById("nav-listen").classList.add("nav-item-active");
+    document.getElementById("nav-about").classList.add("nav-item-minimized");
+  }
+  function navListenInactive(){
+    document.getElementById("nav-listen").classList.remove("nav-item-active");
+    document.getElementById("nav-about").classList.remove("nav-item-minimized");
+  }
+  function navAboutActive(){
+    document.getElementById("nav-about").classList.add("nav-item-active");
+    document.getElementById("nav-listen").classList.add("nav-item-minimized");
+  }
+  function navAboutInactive(){
+    document.getElementById("nav-about").classList.remove("nav-item-active");
+    document.getElementById("nav-listen").classList.remove("nav-item-minimized");
+  }
+}
