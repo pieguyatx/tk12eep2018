195627fbe276b8f20847b4d3da50b99b88bce121
Anthony Brown <abrown4123@gmail.com>
2018-10-09 13:27:26 -0500

    Add EpisodeSearch section to Navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ListenButton from '../ListenButton';
+import EpisodeSearch from '../EpisodeSearch';
 
 const Navbar = () => {
   return (
-    <nav className='nav-primary-container'>
+    <nav className="nav-primary-container">
       <div className="nav-main-item" id="btn-nav-listen">
         <ListenButton />
+        <div className="submenu">
+          <EpisodeSearch />
+        </div>
       </div>
     </nav>
   );
