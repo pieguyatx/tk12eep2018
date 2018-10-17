80dd7f0ba2fcd040c57d099a2a576e685bc76d4f
Anthony Brown <abrown4123@gmail.com>
2018-10-17 17:58:09 -0500

    Add JavaScript animations

--- client/src/components/Header/Navbar/index.js
+++ client/src/components/Header/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ListenButton from '../ListenButton';
 import EpisodeSearch from '../EpisodeSearch';
 import SubscribeList from '../SubscribeList';
@@ -7,26 +7,61 @@ import Tagline from '../Tagline';
 import SocialList from '../SocialList';
 import AboutList from '../AboutList';
 
-const Navbar = () => {
-  return (
-    <nav className="nav-primary-container">
-      <div className="nav-listen nav-item-container" id="nav-listen">
-        <ListenButton />
-        <div className="submenu">
-          <EpisodeSearch />
-          <SubscribeList />
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navListenActive: false,
+      navAboutActive: false
+    }
+  }
+
+  render() {
+    return (
+      <nav className="nav-primary-container">
+        <div onMouseEnter={() => this.setState({navListenActive: true})}
+          onMouseLeave={() => this.setState({navListenActive: false})}
+          className={
+            this.state.navListenActive                                          //This ternary expression will toggle classes
+            ?                                                                   //depending on whether the nav section is hovered
+            "nav-item-active nav-listen nav-item-container"
+            :
+            this.state.navAboutActive
+            ?
+            "nav-minimized nav-listen nav-item-container"
+            :
+            "nav-listen nav-item-container"}
+          id="nav-listen">
+          <ListenButton />
+          <div className="submenu">
+            <EpisodeSearch />
+            <SubscribeList />
+          </div>
         </div>
-      </div>
-      <div className="nav-about nav-item-container" id="nav-about">
-        <AboutButton />
-        <div className="submenu">
-          <Tagline />
-          <SocialList />
-          <AboutList />
+        <div onMouseEnter={() => this.setState({navAboutActive: true})}
+              onMouseLeave={() => this.setState({navAboutActive: false})}
+              className={
+                this.state.navAboutActive                                       //This ternary expression will toggle classes
+                ?                                                               //depending on whether the nav section is hovered
+                "nav-item-active nav-about nav-item-container"
+                :
+                this.state.navListenActive
+                ?
+                "nav-minimized nav-about nav-item-container"
+                :
+                "nav-about nav-item-container"}
+              id="nav-about">
+          <AboutButton />
+          <div className="submenu">
+            <Tagline />
+            <SocialList />
+            <AboutList />
+          </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
