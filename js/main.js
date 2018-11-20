48b136473d2d44b9510d7ef92988b1424a530625
Pius Wong, www.piuswong.com <pius@pioslabs.com>
2018-11-20 14:06:04 -0600

    Seasons page allows reordering; JS scripts debugged for pages w/o single-ep players

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
+// disable button(s) until page fully loaded
+document.getElementById("dropdown-season-order").disabled = true;
+
+// After page is loaded, allow interactivity
 window.onload = function() {
   // initialize navbar hover effects
   init_navbar();
 
+  // run scripts on episode page
   var episodePage = document.getElementsByClassName("page-episode");
   if (episodePage.length>0){
     // Listen for transcript button click
     set_transcript_button();
     // Add button to go back to top
     window.onscroll = function() {scrollFunction()};
-    console.log("test");
   }
 
-  // resize iframe as needed
-  set_iframe_size()
-  window.onresize = function(){
-    set_iframe_size();
+  // run scripts on season page
+  var seasonsPage = document.getElementsByClassName("page-seasons");
+  if (seasonsPage.length>0){
+    // Listen for order button click
+    set_season_order_button();
+  }
+
+  // run scripts specific to Home and Episode pages
+  var homePage = document.getElementsByClassName("home");
+  if (homePage.length>0 || episodePage.length>0){
+    // resize iframe as needed
+    set_iframe_size()
+    window.onresize = function(){
+      set_iframe_size();
+    }
   }
 
 };
@@ -81,4 +96,24 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// On season order selection, set appropriate class
+function set_season_order_button(){
+  document.getElementById("dropdown-season-order").disabled = false;
+  var select = document.getElementById("dropdown-season-order");
+  var seasons = document.getElementById("seasons-list");
+  select.addEventListener("change", function(){
+    var order = this.value;
+    switch (order) {
+      case "oldest":
+        seasons.classList.remove("newest-first");
+        seasons.classList.add("oldest-first");
+        break;
+      case "newest":
+        seasons.classList.remove("oldest-first");
+        seasons.classList.add("newest-first");
+        break;
+    }
+  });
 }
